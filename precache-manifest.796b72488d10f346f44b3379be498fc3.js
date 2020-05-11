02ab4d6066021d9c56fba2bb9477783cf3dbd477
hyosukkim <yakker71@gmail.com>
2020-05-11 15:30:06 +0900

    Updates

---
+++ precache-manifest.796b72488d10f346f44b3379be498fc3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "555a898fe84bcc1d74441eb1f59e3442",
+    "url": "/nori-chart/index.html"
+  },
+  {
+    "revision": "a75701422c8e77f0673d",
+    "url": "/nori-chart/static/css/2.7a7233c8.chunk.css"
+  },
+  {
+    "revision": "fbfdc808e8d36fb5c1cd",
+    "url": "/nori-chart/static/css/main.455a65aa.chunk.css"
+  },
+  {
+    "revision": "a75701422c8e77f0673d",
+    "url": "/nori-chart/static/js/2.b1baf884.chunk.js"
+  },
+  {
+    "revision": "9ec67867846488a940a67e0a507816a8",
+    "url": "/nori-chart/static/js/2.b1baf884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbfdc808e8d36fb5c1cd",
+    "url": "/nori-chart/static/js/main.dc358c8c.chunk.js"
+  },
+  {
+    "revision": "aeae0f9870c71539b7f3",
+    "url": "/nori-chart/static/js/runtime-main.e2bc86bd.js"
+  },
+  {
+    "revision": "8b4e48ece9181b316f69b2e3e173bdde",
+    "url": "/nori-chart/static/media/chart.8b4e48ec.svg"
+  },
+  {
+    "revision": "754d0d1199162739d69ad8c39eda6355",
+    "url": "/nori-chart/static/media/unfold_less.754d0d11.svg"
+  },
+  {
+    "revision": "a48cfd38745bf665d96db4d158a04e54",
+    "url": "/nori-chart/static/media/unfold_more.a48cfd38.svg"
+  }
+]);
